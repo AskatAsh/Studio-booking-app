3f6137bac3238a273611516976016af9ca8097bf
AskatAsh <abdulaskat@gmail.com>
2025-03-03 18:38:13 +0600

    added responsive dropdown menu with dynamic navlinks in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,35 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navlinks = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "font-bold" : "")}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/studiolist"
+          className={({ isActive }) => (isActive ? "font-bold" : "")}
+        >
+          Studio List
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/bookinglist"
+          className={({ isActive }) => (isActive ? "font-bold" : "")}
+        >
+          Booking List
+        </NavLink>
+      </li>
+    </>
+  );
+
   return (
     <header>
       <div className="navbar bg-base-100 shadow-sm">
@@ -9,34 +38,34 @@ const Navbar = () => {
             StudioBooking
           </NavLink>
         </div>
-        <nav className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "font-bold" : "")}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/studiolist"
-                className={({ isActive }) => (isActive ? "font-bold" : "")}
-              >
-                Studio List
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/bookinglist"
-                className={({ isActive }) => (isActive ? "font-bold" : "")}
-              >
-                Studio Bookings
-              </NavLink>
-            </li>
-          </ul>
+        <nav className="flex-none hidden md:block">
+          <ul className="menu menu-horizontal px-1">{navlinks}</ul>
         </nav>
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-40 p-2 shadow right-0"
+          >
+            {navlinks}
+          </ul>
+        </div>
       </div>
     </header>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
-
 const Home = () => {
   return (
     <>
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <div className="text-center space-y-6 mt-10 max-w-5xl w-11/12 mx-auto">
-        <h1 className="text-5xl font-bold text-gray-800">Welcome to Studio Booking App</h1>
-        <p className="text-gray-700 mb-10">A modern user friendly app where users can search and filter and book studios.</p>
-        <Link to="/studiolist" className="btn btn-primary px-8">See Studio List</Link>
+      <div className="text-center max-w-5xl w-11/12 mx-auto flex items-center justify-center h-svh">
+        <div className="space-y-6">
+          <h1 className="text-3xl sm:text-5xl font-bold text-gray-800">
+            Welcome to Studio Booking App
+          </h1>
+          <p className="text-gray-700 mb-10">
+            A modern user friendly app where users can search and filter and
+            book studios.
+          </p>
+          <Link to="/studiolist" className="btn btn-primary px-8">
+            See Studio List
+          </Link>
+        </div>
       </div>
     </>
   );
